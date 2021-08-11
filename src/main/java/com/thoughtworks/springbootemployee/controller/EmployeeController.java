1fbfd576adcff50f0ce54ddcca7a2d4868893f32
CANUTLO <lorenz.alvin.canuto@oocl.com>
2021-08-11 15:32:52 +0800

    [Employee][Gerarld][Lorenz] feat : implemented getEmployeesByPage method

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -1,13 +1,13 @@
 package com.thoughtworks.springbootemployee.controller;
 
+import com.thoughtworks.springbootemployee.model.Directory;
 import com.thoughtworks.springbootemployee.model.Employee;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.thoughtworks.springbootemployee.model.Page;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/employees")
@@ -18,6 +18,14 @@ public class EmployeeController {
     public EmployeeController(){
         employees.add(new Employee(1,"tom",20,"female",1000));
         employees.add(new Employee(2,"jerry",21,"male",900));
+        employees.add(new Employee(3,"Gerarld",23,"male",1100));
+        employees.add(new Employee(4,"Lorenz",21,"male",800));
+        employees.add(new Employee(5,"Falcon",21,"male",2000));
+        employees.add(new Employee(6,"tomB",20,"female",1000));
+        employees.add(new Employee(7,"jerryB",21,"male",900));
+        employees.add(new Employee(8,"GerarldB",23,"male",1100));
+        employees.add(new Employee(9,"LorenzB",21,"male",800));
+        employees.add(new Employee(10,"FalconB",21,"male",2000));
     }
 
     @GetMapping()
@@ -30,4 +38,14 @@ public class EmployeeController {
         return employees.stream().filter(employee -> employee.getId().equals(employeeID) ).findFirst().orElse( null );
     }
 
+    @GetMapping(params = {"pageNumber","pageSize"})
+    private List<Employee> getEmployeesByPage(@RequestParam("pageNumber") Integer pageNumber,
+                                              @RequestParam("pageSize") Integer pageSize) {
+        int skipCount = (pageNumber - 1) * pageSize;
+        return employees
+                .stream()
+                .skip(skipCount)
+                .limit(pageSize)
+                .collect(Collectors.toList());
+    }
 }
