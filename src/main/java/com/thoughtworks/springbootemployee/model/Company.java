6c14d1ccda2388144ac3eb1a608e83c9b2410755
GERARLD DE GUIA (DEV-ISD-OOCLL/MNL) <DEGUIGE@OOCL.COM>
2021-08-11 22:53:31 +0800

    [SpringBootExercise - Employee / Company][Gerarld] feat : created company classes (repository, service, controller)

---
+++ src/main/java/com/thoughtworks/springbootemployee/model/Company.java
@@ -0,0 +1,43 @@
+package com.thoughtworks.springbootemployee.model;
+
+import java.util.List;
+
+public class Company {
+    private Integer id;
+    private String name;
+    private List<Employee> employees;
+
+    public Company(){
+
+    }
+
+    public Company(int companyID, String companyName, List<Employee> employees) {
+        this.id = companyID;
+        this.name = companyName;
+        this.employees = employees;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+}
