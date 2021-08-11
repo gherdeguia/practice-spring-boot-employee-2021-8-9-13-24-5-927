c4af4d8476abb15f5b4606c84f9ddd421e9616c7
GERARLD DE GUIA (DEV-ISD-OOCLL/MNL) <DEGUIGE@OOCL.COM>
2021-08-11 14:51:24 +0800

    [Employee][Gerarld][Lorenz] feat : implemented findEmployeeById method

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -2,6 +2,7 @@ package com.thoughtworks.springbootemployee.controller;
 
 import com.thoughtworks.springbootemployee.model.Employee;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +23,11 @@ public class EmployeeController {
     @GetMapping()
     private List<Employee> getAllEmployees(){
         return employees;
+    }
+
+    @GetMapping(path = "/{employeeID}")
+    private Employee findByEmployeeID(@PathVariable int employeeID){
+        return employees.stream().filter(employee -> employee.getId().equals(employeeID) ).findFirst().orElse( null );
     }
 
 }
